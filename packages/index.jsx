78331061cd82288f919b55f96075b430cda6c37e
xing-6464 <17621404964@163.com>
2024-07-21 06:47:49 +0800

    修改

--- packages/index.jsx
+++ packages/index.jsx
@@ -7,12 +7,12 @@ function FunctionComponent() {
             console.log(`ParentNodeCapture`)
             
         }}>
-        <div>课程名称：手写React高质量源码迈向高阶开发</div>
+        <div>课程名称：</div>
         <div onClick={(event) => {
           console.log(`ChildNodeBubble`);
           
         }}
-        onClickCapture={(event) => console.log(`ChildNodeCapture`)}>讲师：杨艺韬</div>
+        onClickCapture={(event) => console.log(`ChildNodeCapture`)}>讲师</div>
         <div>电子书：<a style={{ color: 'blue' }} href="https://www.yangyitao.com/react18">https://www.yangyitao.com/react18</a></div>
     </div>
 }
